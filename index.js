91b5d673d3d7c7cb398c81810d467e446c9eac29
ordinaryYT <baileybailey698@gmail.com>
2025-04-16 11:08:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,89 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, getVoiceConnection } = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
+import discord
+from discord.ext import commands
+import youtube_dl
+import asyncio
+import os
+from dotenv import load_dotenv
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+# Load environment variables from .env
+load_dotenv()
 
-const player = createAudioPlayer();
+# Bot setup
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix="!", intents=intents)
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+# Global variables for the bot state
+vc = None
+song_queue = []
 
-client.on('messageCreate', async message => {
-    if (!message.guild) return;
+# FFmpeg options for audio streaming
+ffmpeg_opts = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn',
+}
 
-    const args = message.content.split(' ');
-    const command = args[0];
+# Join the voice channel
+@client.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        vc = await channel.connect()
+    else:
+        await ctx.send("You must join a voice channel first!")
 
-    if (command === '!play') {
-        if (!args[1]) return message.reply('Please provide a YouTube link!');
-        const voiceChannel = message.member.voice.channel;
-        if (!voiceChannel) return message.reply('Join a voice channel first!');
+# Leave the voice channel
+@client.command()
+async def leave(ctx):
+    if vc:
+        await vc.disconnect()
+    else:
+        await ctx.send("I'm not in a voice channel!")
 
-        const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: message.guild.id,
-            adapterCreator: message.guild.voiceAdapterCreator,
-        });
-
-        const stream = ytdl(args[1], { filter: 'audioonly' });
-        const resource = createAudioResource(stream);
-        player.play(resource);
-        connection.subscribe(player);
-
-        message.reply('Now playing!');
+# Play a song from YouTube
+@client.command()
+async def play(ctx, *, song: str):
+    if not ctx.author.voice:
+        await ctx.send("You need to join a voice channel first!")
+        return
+    
+    channel = ctx.author.voice.channel
+    vc = await channel.connect()
+    
+    # Fetch the song URL from YouTube
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'audioquality': 1,
+        'outtmpl': 'downloads/%(id)s.%(ext)s',
+        'restrictfilenames': True,
+        'noplaylist': True,
+        'quiet': True,
+        'logtostderr': False,
     }
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f'ytsearch:{song}', download=False)
+        url = info['entries'][0]['url']
+        
+    # Play the song
+    vc.play(discord.FFmpegPCMAudio(url, **ffmpeg_opts), after=lambda e: print('done', e))
 
-    if (command === '!stop') {
-        player.stop();
-        message.reply('Playback stopped.');
-    }
+    await ctx.send(f"Now playing: {song}")
 
-    if (command === '!leave') {
-        const connection = getVoiceConnection(message.guild.id);
-        if (connection) {
-            connection.destroy();
-            message.reply('Left the voice channel.');
-        } else {
-            message.reply('Not in a voice channel.');
-        }
-    }
-});
+# Skip the current song
+@client.command()
+async def skip(ctx):
+    if vc and vc.is_playing():
+        vc.stop()
+        await ctx.send("Song skipped!")
+    else:
+        await ctx.send("No song is currently playing.")
 
-client.login(process.env.DISCORD_TOKEN);
+# Listens to the message content and handle the music
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+# Run the bot with your token from environment variable
+client.run(os.getenv('DISCORD_TOKEN'))
